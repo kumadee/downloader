812c993833c6ad474d51144b2248be962d4e0e51
kumadee <deepak.kumar.ext@gmail.com>
2021-03-27 12:58:04 +0000

    refactor: run tests on all platforms

--- pkg/http/download_test.go
+++ pkg/http/download_test.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,21 +12,21 @@ import (
 func TestDownloadFile(t *testing.T) {
 	var cases = []struct {
 		url          string
-		path         string
 		expectedFile string
 		description  string
 	}{
 		{
 			url:          "http://public.animeout.xyz/nimbus.animeout.com/series/Ongoing/Overlord Ple Ple Pleiades/[AnimeOut] Overlord Ple Ple Pleiades - 01 - Special [720p][AAC][DeadFish][RapidBot].mkv",
-			path:         "/tmp/overlord-specials",
-			expectedFile: "/tmp/overlord-specials/[AnimeOut] Overlord Ple Ple Pleiades - 01 - Special [720p][AAC][DeadFish][RapidBot].mkv",
+			expectedFile: "[AnimeOut] Overlord Ple Ple Pleiades - 01 - Special [720p][AAC][DeadFish][RapidBot].mkv",
 			description:  "Download file at non-existent path",
 		},
 	}
 	for _, tc := range cases {
-		err := DownloadFile(tc.path, tc.url)
+		path, err := os.MkdirTemp("", "")
+		require.NoError(t, err, "Create temp directory")
+		err = DownloadFile(path, tc.url)
 		require.NoError(t, err, tc.description)
-		assert.DirExists(t, tc.path, tc.description)
-		assert.FileExists(t, tc.expectedFile, tc.description)
+		assert.DirExists(t, path, tc.description)
+		assert.FileExists(t, filepath.Join(path, tc.expectedFile), tc.description)
 	}
 }
